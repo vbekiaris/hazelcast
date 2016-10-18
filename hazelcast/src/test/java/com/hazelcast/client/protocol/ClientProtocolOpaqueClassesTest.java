2b43aefb8e7af49f9bdb42ede0f07699abb80309
Vassilis Bekiaris <vassilis@hazelcast.com>
2016-10-18 15:59:46 +0300

    More tests for data serializable / IdentifiedDataSerializable

--- hazelcast/src/test/java/com/hazelcast/client/protocol/ClientProtocolOpaqueClassesTest.java
+++ hazelcast/src/test/java/com/hazelcast/client/protocol/ClientProtocolOpaqueClassesTest.java
@@ -16,29 +16,41 @@
 
 package com.hazelcast.client.protocol;
 
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.LinkedListMultimap;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.TreeMultimap;
 import com.hazelcast.client.impl.protocol.ClientProtocol;
 import com.hazelcast.nio.serialization.DataSerializable;
 import com.hazelcast.nio.serialization.IdentifiedDataSerializable;
 import com.hazelcast.test.HazelcastParallelClassRunner;
+import com.hazelcast.test.HazelcastSerialClassRunner;
 import com.hazelcast.test.annotation.ParallelTest;
 import com.hazelcast.test.annotation.QuickTest;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 import org.junit.runner.RunWith;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.SortedSet;
 import java.util.TreeSet;
 
 import static com.hazelcast.test.ReflectionsHelper.REFLECTIONS;
+import static com.hazelcast.test.ReflectionsHelper.TEST_CLASSES_CLASSLOADER;
+import static com.hazelcast.test.ReflectionsHelper.isTestClass;
 import static org.junit.Assert.fail;
 
 /**
  * Tests for classes which are used as Data in the client protocol
  */
-@RunWith(HazelcastParallelClassRunner.class)
-@Category({QuickTest.class, ParallelTest.class})
+@RunWith(HazelcastSerialClassRunner.class)
+@Category({QuickTest.class})
 public class ClientProtocolOpaqueClassesTest {
 
     // verify that any class which is DataSerializable and is not annotated with @ClientProtocol
@@ -53,13 +65,13 @@ public class ClientProtocolOpaqueClassesTest {
         dataSerializableClasses.remove(IdentifiedDataSerializable.class);
 
         // locate all classes annotated with ClientProtocol (and their subclasses) and remove those as well
-        Set allAnnotatedClasses = REFLECTIONS.getTypesAnnotatedWith(ClientProtocol.class);
+        Set allAnnotatedClasses = REFLECTIONS.getTypesAnnotatedWith(ClientProtocol.class, false);
         dataSerializableClasses.removeAll(allAnnotatedClasses);
 
         // since test classes are included in the classpath, remove them
         Set testClasses = new HashSet();
         for (Object o : dataSerializableClasses) {
-            if (o.toString().contains("Test") || o.toString().contains("Mock")) {
+            if (isTestClass((Class) o)) {
                 testClasses.add(o);
             }
         }
@@ -78,6 +90,79 @@ public class ClientProtocolOpaqueClassesTest {
             }
             fail("There are " + dataSerializableClasses.size() + " classes which are DataSerializable, not @ClientProtocol-"
                     + "annotated and are not IdentifiedDataSerializable.");
+        }
+    }
+
+    @Test
+    public void test_subclassesOfAnnotatedClasses_areAnnotated() {
+        // locate all classes annotated with ClientProtocol (and their subclasses) and remove those as well
+        Set<Class<?>> directlyAnnotatedClasses = REFLECTIONS.getTypesAnnotatedWith(ClientProtocol.class, true);
+        Set<String> notAnnotatedSubclasses = new TreeSet<String>();
+
+        for (Class klass : directlyAnnotatedClasses) {
+            Set subclasses = REFLECTIONS.getSubTypesOf(klass);
+            for (Object subclass : subclasses) {
+                if (!((Class) subclass).isAnnotationPresent(ClientProtocol.class) && !isTestClass((Class) subclass)) {
+                    notAnnotatedSubclasses.add(subclass.toString());
+                }
+            }
+        }
+
+        if (!notAnnotatedSubclasses.isEmpty()) {
+            System.out.println("The following classes are subclasses of an annotated interface or class but do not bear the "
+                    + "annotation themselves");
+            for (String s : notAnnotatedSubclasses) {
+                System.out.println(s);
+            }
+            fail("There are " + notAnnotatedSubclasses.size() + " subclasses of @ClientProtocol annotated classes");
+        }
+    }
+
+    @Test
+    public void test_identifiedDataSerializables_haveUniqueFactoryAndTypeId()
+            throws IllegalAccessException, InvocationTargetException {
+        Set<String> classesWithInstantiationProblems = new TreeSet<String>();
+
+        Multimap<Integer, Integer> factoryToTypeId = HashMultimap.create();
+
+        Set<Class<? extends IdentifiedDataSerializable>> identifiedDataSerializables = REFLECTIONS
+                .getSubTypesOf(IdentifiedDataSerializable.class);
+
+        for (Class<? extends IdentifiedDataSerializable> klass : identifiedDataSerializables) {
+            // only test production classes
+            if (!isTestClass(klass) && !klass.isInterface() && !Modifier.isAbstract(klass.getModifiers())) {
+                // wrap all of this in try-catch, as it is legitimate for some classes to throw UnsupportedOperationException
+                try {
+                    Constructor<? extends IdentifiedDataSerializable> ctor = klass.getDeclaredConstructor();
+                    ctor.setAccessible(true);
+                    IdentifiedDataSerializable instance = ctor.newInstance();
+                    int factoryId = instance.getFactoryId();
+                    int typeId = instance.getId();
+                    if (factoryToTypeId.containsEntry(factoryId, typeId)) {
+                        fail("Factory-Type ID pair {" + factoryId + ", " + typeId + "} from " + klass.toString() + " is already "
+                                + "registered in another type.");
+                    } else {
+                        factoryToTypeId.put(factoryId, typeId);
+                    }
+                } catch (UnsupportedOperationException e) {
+                    // expected from some classes
+                } catch (InstantiationException e) {
+                    classesWithInstantiationProblems.add(klass.getName());
+                    e.printStackTrace();
+                } catch (NoSuchMethodException e) {
+                    classesWithInstantiationProblems.add(klass.getName());
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        if (!classesWithInstantiationProblems.isEmpty()) {
+            System.out.println("There are " + classesWithInstantiationProblems.size() + " classes which threw an exception while "
+                    + "attempting to invoke a default no-args constructor. See console output for exception details. List of "
+                    + "problematic classes:");
+            for (String className : classesWithInstantiationProblems) {
+                System.out.println(className);
+            }
         }
     }
 
--- hazelcast/src/test/java/com/hazelcast/test/ReflectionsHelper.java
+++ hazelcast/src/test/java/com/hazelcast/test/ReflectionsHelper.java
@@ -18,17 +18,74 @@ package com.hazelcast.test;
 import org.reflections.Reflections;
 import org.reflections.scanners.SubTypesScanner;
 import org.reflections.scanners.TypeAnnotationsScanner;
+import org.reflections.util.ClasspathHelper;
 import org.reflections.util.ConfigurationBuilder;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 /**
- * Initialize reflections to avoid duplicate work scanning the classpath
+ * Initialize once org.reflections library to avoid duplicate work scanning the classpath from individual tests.
+ * Reflections is configured with the {@code TEST_CLASSES_CLASSLOADER}. A quick way to test whether a {@code Class} is loaded
+ * from production or test classes is to check whether {code klass.getClassLoader().equals(TEST_CLASSES_CLASSLOADER)} is true.
  */
 public class ReflectionsHelper {
 
     public static final Reflections REFLECTIONS;
+    // classes in the test classpath are loaded by this classloader, which delegates to the parent classloader _after_ attempting
+    // to load the classes itself.
+    public static final ClassLoader TEST_CLASSES_CLASSLOADER;
 
     static {
-        REFLECTIONS = new Reflections(new ConfigurationBuilder().forPackages("com.hazelcast")
+        File testClassesDirectory = new File("target/test-classes");
+
+        URL productionClassesURL = null;
+        try {
+            productionClassesURL = testClassesDirectory.toURI().toURL();
+        } catch (MalformedURLException e) {
+            throw new AssertionError(
+                    "Could not obtain a URL for the compiled test classes classpath at " + testClassesDirectory
+                            .getAbsolutePath());
+        }
+
+        TEST_CLASSES_CLASSLOADER = new PostDelegationClassLoader(new URL[]{productionClassesURL},
+                ClasspathHelper.staticClassLoader());
+        REFLECTIONS = new Reflections(new ConfigurationBuilder()
+                .addUrls(ClasspathHelper.forPackage("com.hazelcast", TEST_CLASSES_CLASSLOADER))
+                .addClassLoader(TEST_CLASSES_CLASSLOADER)
                 .addScanners(new SubTypesScanner()).addScanners(new TypeAnnotationsScanner()));
+    }
+
+    // a URLClassLoader that delegates to the parent classloader _after_ having attempted to
+    // locate the class in its configured URLs
+    public static class PostDelegationClassLoader extends URLClassLoader {
+
+        public PostDelegationClassLoader(URL[] urls, ClassLoader parent) {
+            super(urls, parent);
+        }
+
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            Class loadedClass = findLoadedClass(name);
+            if (loadedClass == null) {
+                try {
+                    // first try loading the class from the configured URLs
+                    loadedClass = findClass(name);
+                } catch (ClassNotFoundException e) {
+                }
+
+                // If not found locally, delegate to parent via URLClassLoader.loadClass
+                if (loadedClass == null) {
+                    // do not catch ClassNotFoundException here
+                    loadedClass = super.loadClass(name);
+                }
+            }
+            return loadedClass;
+        }
+    }
+
+    public static boolean isTestClass(Class klass) {
+        return klass.getClassLoader().equals(TEST_CLASSES_CLASSLOADER);
     }
 }
