e7827f9961ef6083cc3c739a149576ff14d6f955
Jaromir Hamala <jaromir.hamala@gmail.com>
2016-12-07 15:41:16 +0200

    wip

--- hazelcast/src/main/java/com/hazelcast/client/impl/protocol/DefaultMessageTaskFactoryProvider.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/protocol/DefaultMessageTaskFactoryProvider.java
@@ -1803,32 +1803,32 @@ public class DefaultMessageTaskFactoryProvider implements MessageTaskFactoryProv
         //region ----------  REGISTRATION FOR com.hazelcast.client.impl.protocol.task.enterprisemap
         factories[com.hazelcast.client.impl.protocol.codec.EnterpriseMapDestroyCacheCodec.RequestParameters.TYPE.id()] = new MessageTaskFactory() {
             public MessageTask create(ClientMessage clientMessage, Connection connection) {
-                return new com.hazelcast.client.impl.protocol.task.enterprisemap.MapDestroyCacheMessageTask(clientMessage, node, connection);
+                return new com.hazelcast.client.impl.protocol.task.map.MapDestroyCacheMessageTask(clientMessage, node, connection);
             }
         };
         factories[com.hazelcast.client.impl.protocol.codec.EnterpriseMapPublisherCreateCodec.RequestParameters.TYPE.id()] = new MessageTaskFactory() {
             public MessageTask create(ClientMessage clientMessage, Connection connection) {
-                return new com.hazelcast.client.impl.protocol.task.enterprisemap.MapPublisherCreateMessageTask(clientMessage, node, connection);
+                return new com.hazelcast.client.impl.protocol.task.map.MapPublisherCreateMessageTask(clientMessage, node, connection);
             }
         };
         factories[com.hazelcast.client.impl.protocol.codec.EnterpriseMapSetReadCursorCodec.RequestParameters.TYPE.id()] = new MessageTaskFactory() {
             public MessageTask create(ClientMessage clientMessage, Connection connection) {
-                return new com.hazelcast.client.impl.protocol.task.enterprisemap.MapSetReadCursorMessageTask(clientMessage, node, connection);
+                return new com.hazelcast.client.impl.protocol.task.map.MapSetReadCursorMessageTask(clientMessage, node, connection);
             }
         };
         factories[com.hazelcast.client.impl.protocol.codec.EnterpriseMapAddListenerCodec.RequestParameters.TYPE.id()] = new MessageTaskFactory() {
             public MessageTask create(ClientMessage clientMessage, Connection connection) {
-                return new com.hazelcast.client.impl.protocol.task.enterprisemap.MapAddListenerMessageTask(clientMessage, node, connection);
+                return new com.hazelcast.client.impl.protocol.task.map.MapAddListenerMessageTask(clientMessage, node, connection);
             }
         };
         factories[com.hazelcast.client.impl.protocol.codec.EnterpriseMapMadePublishableCodec.RequestParameters.TYPE.id()] = new MessageTaskFactory() {
             public MessageTask create(ClientMessage clientMessage, Connection connection) {
-                return new com.hazelcast.client.impl.protocol.task.enterprisemap.MapMadePublishableMessageTask(clientMessage, node, connection);
+                return new com.hazelcast.client.impl.protocol.task.map.MapMadePublishableMessageTask(clientMessage, node, connection);
             }
         };
         factories[com.hazelcast.client.impl.protocol.codec.EnterpriseMapPublisherCreateWithValueCodec.RequestParameters.TYPE.id()] = new MessageTaskFactory() {
             public MessageTask create(ClientMessage clientMessage, Connection connection) {
-                return new com.hazelcast.client.impl.protocol.task.enterprisemap.MapPublisherCreateWithValueMessageTask(clientMessage, node, connection);
+                return new com.hazelcast.client.impl.protocol.task.map.MapPublisherCreateWithValueMessageTask(clientMessage, node, connection);
             }
         };
 //endregion
