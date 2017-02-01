00356d395d05cd92dc463a153c33fceb9dc4e982
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-02-01 11:28:41 +0300

    broken proxy chain added

--- src/main/java/info/jerrinot/compatibilityguardian/HazelcastProxyFactory.java
+++ src/main/java/info/jerrinot/compatibilityguardian/HazelcastProxyFactory.java
@@ -10,13 +10,27 @@ import static info.jerrinot.compatibilityguardian.Utils.rethrow;
 
 public class HazelcastProxyFactory {
     public static HazelcastInstance proxy(Object hazelcastDelegate) {
-        if (!checkImplementInterface(hazelcastDelegate, HazelcastInstance.class)) {
-            throw new GuardianException("Cannot create proxy for class " + hazelcastDelegate);
-        }
-        InvocationHandler myInvocationHandler = new MyInvocationHandler(hazelcastDelegate);
-        ClassLoader classloader = HazelcastProxyFactory.class.getClassLoader();
-        HazelcastInstance proxy = (HazelcastInstance) Proxy.newProxyInstance(classloader, new Class[]{HazelcastInstance.class}, myInvocationHandler);
+        Class<HazelcastInstance> expectedInterface = HazelcastInstance.class;
+        HazelcastInstance proxy = generateProxyForInterface(hazelcastDelegate, expectedInterface);
         return proxy;
+    }
+
+    private static <T> T generateProxyForInterface(Object delegate, Class<?>...expectedInterfaces) {
+        if (!checkImplementInterfaces(delegate, expectedInterfaces)) {
+            throw new GuardianException("Cannot create proxy for class " + delegate);
+        }
+        InvocationHandler myInvocationHandler = new MyInvocationHandler(delegate);
+        ClassLoader classloader = HazelcastProxyFactory.class.getClassLoader();
+        return (T) Proxy.newProxyInstance(classloader, expectedInterfaces, myInvocationHandler);
+    }
+
+    private static boolean checkImplementInterfaces(Object o, Class<?>...ifaces) {
+//        for (Class<?> iface : ifaces) {
+//            if (!checkImplementInterface(o, iface)) {
+//                return false;
+//            }
+//        }
+        return true;
     }
 
     private static boolean checkImplementInterface(Object o, Class<?> iface) {
@@ -32,23 +46,31 @@ public class HazelcastProxyFactory {
     private static class MyInvocationHandler implements InvocationHandler {
         private final Object delegate;
 
-        private final Method shutdownMethod;
 
         private MyInvocationHandler(Object delegate) {
             this.delegate = delegate;
-            Class<?> delegateClass = delegate.getClass();
-            try {
-                this.shutdownMethod = delegateClass.getMethod("shutdown");
-            } catch (NoSuchMethodException e) {
-                throw rethrow(e);
-            }
         }
 
         @Override
         public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+            Class<?> returnType = method.getReturnType();
+
             Class<?> aClass = delegate.getClass();
             Method methodDelegate = aClass.getMethod(method.getName(), method.getParameterTypes());
-            return methodDelegate.invoke(delegate, args);
+            Object nestedDelegate = methodDelegate.invoke(delegate, args);
+
+            Class<?>[] interfaces = returnType.getInterfaces();
+
+
+            Object resultingProxy = HazelcastProxyFactory.generateProxyForInterface(nestedDelegate, interfaces);
+            System.out.println("Returning proxy " + resultingProxy + ", loaded by " + resultingProxy.getClass().getClassLoader());
+            Class<?>[] ifaces = resultingProxy.getClass().getInterfaces();
+            System.out.println("The proxy implementes intefaces: ");
+            for (Class<?> iface : ifaces) {
+                System.out.println(iface + ", loaded by " + iface.getClassLoader());
+            }
+            return resultingProxy;
+//            return null;
         }
     }
 }
