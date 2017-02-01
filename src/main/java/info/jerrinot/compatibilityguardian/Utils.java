2c9a806e405546446340a0edcb5910d21b36c998
Jaromir Hamala <jaromir.hamala@gmail.com>
2017-02-01 14:15:07 +0300

    add cross-classloader cloning

--- src/main/java/info/jerrinot/compatibilityguardian/Utils.java
+++ src/main/java/info/jerrinot/compatibilityguardian/Utils.java
@@ -2,8 +2,14 @@ package info.jerrinot.compatibilityguardian;
 
 import com.hazelcast.util.collection.ArrayUtils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
+import java.io.Serializable;
 import java.lang.reflect.Array;
-import java.util.Arrays;
 
 public class Utils {
     public static final boolean DEBUG_ENABLED = true;
@@ -20,6 +26,30 @@ public class Utils {
         T[] newArray = (T[]) Array.newInstance(componentType, source.length + items.length);
         ArrayUtils.concat(source, items, newArray);
         return newArray;
+    }
+
+    public static Serializable clone(Serializable original, ClassLoader targetClassLoader) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(baos);
+            oos.writeObject(original);
+            byte[] bytes = baos.toByteArray();
+
+            ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
+            ObjectInputStream ois = new ObjectInputStream(bais) {
+                @Override
+                protected Class<?> resolveClass(ObjectStreamClass desc) throws IOException, ClassNotFoundException {
+                    return targetClassLoader.loadClass(desc.getName());
+                }
+            };
+
+            Serializable o = (Serializable) ois.readObject();
+            return o;
+        } catch (IOException e) {
+            throw rethrow(e);
+        } catch (ClassNotFoundException e) {
+            throw rethrow(e);
+        }
     }
 
     public static void debug(String text) {
