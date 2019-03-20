57fe509cb4ef768410dbfe06eb008319bef51ea0
Vassilis Bekiaris <vbekiaris@gmail.com>
2019-03-20 16:04:37 +0200

    WIP Fixes NPE during split-brain

--- hazelcast/src/main/java/com/hazelcast/client/impl/ClientPartitionListenerService.java
+++ hazelcast/src/main/java/com/hazelcast/client/impl/ClientPartitionListenerService.java
@@ -18,7 +18,7 @@ package com.hazelcast.client.impl;
 
 import com.hazelcast.client.impl.protocol.ClientMessage;
 import com.hazelcast.client.impl.protocol.codec.ClientAddPartitionListenerCodec;
-import com.hazelcast.instance.EndpointQualifier;
+import com.hazelcast.core.Member;
 import com.hazelcast.internal.partition.PartitionReplica;
 import com.hazelcast.internal.partition.PartitionTableView;
 import com.hazelcast.nio.Address;
@@ -31,6 +31,8 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import static com.hazelcast.instance.EndpointQualifier.CLIENT;
 
 public class ClientPartitionListenerService {
 
@@ -105,6 +107,15 @@ public class ClientPartitionListenerService {
         if (!advancedNetworkConfigEnabled) {
             return memberAddress;
         }
-        return nodeEngine.getClusterService().getMember(memberAddress).getAddressMap().get(EndpointQualifier.CLIENT);
+        Member member = nodeEngine.getClusterService().getMember(memberAddress);
+        if (member != null) {
+            return member.getAddressMap().get(CLIENT);
+        } else {
+            // partition table seems to contain stale entries
+            // the member address will be wrong but send it anyway because here is no
+            // better information available at the moment
+            // once partition table is updated, the correct addresses will be delivered
+            return memberAddress;
+        }
     }
 }
