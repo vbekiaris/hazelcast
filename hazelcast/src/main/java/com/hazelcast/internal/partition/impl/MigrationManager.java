9d95a2781c2ab21ea5f0ca5bd00af1d67d5a50c3
Vassilis Bekiaris <vbekiaris@gmail.com>
2021-06-04 10:34:30 +0300

    Fixes migration succeeded callback execution from FJP#commonPool
    Instead of relying on the shared common pool,
    internal operations should use dedicated
    async executor.

--- hazelcast/src/main/java/com/hazelcast/internal/partition/impl/MigrationManager.java
+++ hazelcast/src/main/java/com/hazelcast/internal/partition/impl/MigrationManager.java
@@ -87,6 +87,7 @@ import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
@@ -1521,6 +1522,7 @@ public class MigrationManager {
             migrationInterceptor.onMigrationComplete(MigrationParticipant.MASTER, migration, true);
             long start = System.nanoTime();
 
+            Executor executor = nodeEngine.getExecutionService().getExecutor(ASYNC_EXECUTOR);
             CompletionStage<Boolean> f = commitMigrationToDestinationAsync(migration);
             f = f.thenApplyAsync(commitSuccessful -> {
                 stats.recordDestinationCommitTime(System.nanoTime() - start);
@@ -1572,7 +1574,7 @@ public class MigrationManager {
                     partitionServiceLock.unlock();
                 }
                 return commitSuccessful;
-            });
+            }, executor);
             return f;
         }
     }
